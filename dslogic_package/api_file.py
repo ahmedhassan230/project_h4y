38028ad48e90b1259d129640e2f3449aa71eefb4
Srividhya Leeladharan <srividhya.leeladharan@gmail.com>
2024-06-03 11:45:09 +0200

    updated the output

--- dslogic_package/api_file.py
+++ dslogic_package/api_file.py
@@ -42,8 +42,14 @@ async def predict(usr:User):
             return 'Underweight'
         else:
             return 'Normal'
-    
+
     try:
+        user_cat_dict= {"diabetic":"NO",
+                        "heart":"NO",
+                        "osteoporosis":"NO",
+                        "mental_health":"NO",
+                        "generic":"NO"}
+
         user_dict = usr.dict()
         df = pd.DataFrame([user_dict])
         user_category=[]
@@ -55,35 +61,41 @@ async def predict(usr:User):
                         'sleeping_hrs':'Sleep Hours Per Day','age':'Age','weight':'Weight','height':'Height'}, inplace=True)
         df['BMI']= df['Weight']/df['Height']**2
         #Define Body Weight
-        
+
         df['Body Weight'] = df['BMI'].apply(categorize_bmi)
-        
+
         #call the diabetes logic
         diabetes_pred=diabetes_logic.diabetes_outcome(df[['Age','BMI']])
         if diabetes_pred !=None:
             user_category.append(diabetes_pred)
+            user_cat_dict["diabetic"]="YES"
         #call the heartattack logic
         heart_pred=heart_attack_logic.hrt_attack_outcome(df[['Sex','Age','Smoking','Alcohol Consumption','Sleep Hours Per Day','BMI']])
         if heart_pred !=None:
             user_category.append(heart_pred)
+            user_cat_dict["heart"]="YES"
 
         #call the Osteoperosis logic
         #'Body Weight', 'Calcium Intake', 'Vitamin D Intake', 'Physical Activity', 'Smoking', 'Alcohol Consumption'
-        
+
         osteoporosis_pred=osteoporosis_logic.osteoporosis_model(df[['Body Weight', 'Calcium Intake', 'Vitamin D Intake', 'Physical Activity', 'Smoking', 'Alcohol Consumption']])
         if osteoporosis_pred !=None:
-            user_category.append(osteoporosis_pred) 
-        
+            user_category.append(osteoporosis_pred)
+            user_cat_dict["osteoporosis"]="YES"
+
         #call the Mental Health logic
         df.rename(columns={'Sex':'Gender'}, inplace = True)
         mental_health_pred=mental_health_logic.mental_model(df[['Gender', 'Occupation', 'self_employed', 'Days_Indoors', 'Country']])
 
         if mental_health_pred!=None:
             user_category.append(mental_health_pred)
+            user_cat_dict["mental_health"]="YES"
 
         if user_category==[]:
-            user_category.append('Generic')
-        return user_category
+            user_category.append('generic')
+            user_cat_dict["generic"]="YES"
+
+        return user_cat_dict
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
