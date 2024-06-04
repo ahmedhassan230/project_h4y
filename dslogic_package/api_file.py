6c3cf407cb8867ba44a4ff2dca176df855935e8a
Srividhya Leeladharan <srividhya.leeladharan@gmail.com>
2024-06-04 14:37:45 +0200

    add post catgorise logic

--- dslogic_package/api_file.py
+++ dslogic_package/api_file.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from dslogic_package.ml_logic import diabetes_logic
-from dslogic_package.ml_logic import heart_attack_logic
 from pydantic import BaseModel
-from dslogic_package.ml_logic import mental_health_logic
-from dslogic_package.ml_logic import osteoporosis_logic
+from dslogic_package.ml_logic import user_categoriztion
+from dslogic_package.ml_logic import recommend_posts
 import pandas as pd
-from dataclasses import dataclass, asdict
+
 
 class User(BaseModel):
     occupation: str
@@ -37,70 +35,21 @@ def read_root():
 @app.post("/categorize")
 
 async def predict(usr:User):
-    def categorize_bmi(bmi):
-        if bmi < 18.5:
-            return 'Underweight'
-        else:
-            return 'Normal'
-
 
     try:
-        user_cat_dict= {"diabetic":"NO",
-                        "heart":"NO",
-                        "osteoporosis":"NO",
-                        "mental_health":"NO",
-                        "generic":"NO"}
+        return user_categoriztion.user_category(usr)
 
-        user_dict = usr.dict()
-        df = pd.DataFrame([user_dict])
-        user_category=[]
-        # Rename columns
-        df.rename(columns={'occupation': 'Occupation', 'gender': 'Sex',
-                        'country': 'Country','days_indoors':'Days_Indoors',
-                        'smoking':'Smoking','alcohol_consumption': 'Alcohol Consumption',
-                        'sun_exposure': 'Vitamin D Intake','activity': 'Physical Activity','dairy_intake':'Calcium Intake',
-                        'sleeping_hrs':'Sleep Hours Per Day','age':'Age','weight':'Weight','height':'Height'}, inplace=True)
-        df['BMI']= df['Weight']*10000/df['Height']**2
-
-        #Define Body Weight
-        df['Body Weight'] = df['BMI'].apply(categorize_bmi)
-
-        #call the diabetes logic
-        diabetes_pred=diabetes_logic.diabetes_outcome(df[['Age','BMI']])
-        if diabetes_pred !=None:
-            user_category.append(diabetes_pred)
-            user_cat_dict["diabetic"]="YES"
-
-        #call the Osteoperosis logic
-        #'Body Weight', 'Calcium Intake', 'Vitamin D Intake', 'Physical Activity', 'Smoking', 'Alcohol Consumption'
-
-        osteoporosis_pred=osteoporosis_logic.osteoporosis_model(df[['Body Weight', 'Calcium Intake', 'Vitamin D Intake', 'Physical Activity', 'Smoking', 'Alcohol Consumption']])
-
-        if osteoporosis_pred !=None:
-            user_category.append(osteoporosis_pred)
-            user_cat_dict["osteoporosis"]="YES"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-        heart_pred=heart_attack_logic.hrt_attack_outcome(df[['Sex','Age','Smoking','Alcohol Consumption','Sleep Hours Per Day','BMI']])
+#Adding Logic to return the Posts
 
-        if heart_pred !=None:
-            user_category.append(heart_pred)
-            user_cat_dict["heart"]="YES"
-
-        #call the Mental Health logic
-
-        df.rename(columns={'Sex':'Gender'}, inplace = True)
-        mental_health_pred=mental_health_logic.mental_model(df[['Gender', 'Occupation', 'self_employed', 'Days_Indoors', 'Country']])
-
-        if mental_health_pred!=None:
-            user_category.append(mental_health_pred)
-            user_cat_dict["mental_health"]="YES"
-
-        if user_category==[]:
-            user_category.append('generic')
-            user_cat_dict["generic"]="YES"
-
-        return user_cat_dict
+@app.post("/posts")
+async def post_recommendation(usr:User):
+    try:
+        user_category= user_categoriztion.user_category(usr)
+        return recommend_posts.post_finder(user_category)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
---
+++ dslogic_package/ml_logic/recommend_posts.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def post_finder(user):
+
+    return "random posts"
