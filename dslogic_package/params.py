21b9f6b257943e88963c0dd846a333167e3edabd
Srividhya Leeladharan <srividhya.leeladharan@gmail.com>
2024-06-06 11:34:42 +0200

    updated the tag list

--- dslogic_package/params.py
+++ dslogic_package/params.py
@@ -4,15 +4,11 @@ LOCAL_PATH=os.path.abspath(__file__)
 LOCAL_DIR=os.path.dirname(LOCAL_PATH)
 MODEL_DIR=os.path.join(LOCAL_DIR,"models")
 PREPRO_DIR=os.path.join(LOCAL_DIR,"ml_logic/preprocessors")
-TAG_LIST=['vaccines','therapy','muscles','routine','wellbeing','selfcare','bones','fracture','pain','arthritis','cholesterol','stress','mindfulness','depression','mood','osteoporosis','challenges','lifestyle','healthy','diet','greens','wellbeing','handwashing','cleanliness','aging','vitamins','joints','health','calcium','insulin','workout','cardio','exercise','checkups','recovery','distress', 'tension','anxiety', 'fatigue', 'pressure', 'relaxation','Calm','Peaceful','Tranquil','Serene','Composed','Carefree','Easygoing','Laidback','Meditation','Yoga','Vacation','Spa','Massage','Reading','NatureWalks','Bathing','BreathingExercises','Mindfulness','Aromatherapy','Unwind','glucose','diabetes','nutrition','insulin', 'hypoglycaemia','chest pain','heartattack', 'heart','CVD', 'blood pressure', 'heart failure', 'heart attack', 'pain', 'fracture', 'back pain', 'joint pain', 'trauma', 'bone', 'bone density', 'bone loss', 'muscle pain', 'Sports', 'injury','cardiovascular diseases','IntermittentFasting','KetoDiet','Superfoods','OrganicFoods','Vegan','nutrition','worklifebalance','hygienepractices','acupuncture','minimalism']
+TAG_LIST=["Anxiety", "Stress", "Depression", "Mindfulness", "Wellness", "Mental Health Therapy", "Resilience", "Self-care", "Psychology", "Happiness", "Diabetes Prevention", "Glucose", "Insulin", "Diet", "Glycemic", "Fitness", "Foot Care", "Type 1 Diabetes", "Type 2 Diabetes", "Cardio", "Heart Diet", "Cholesterol", "Blood Pressure", "Heart Care", "Physical Rehab", "Stress", "Surgery", "Heart Rate", "Heart Health", "Density", "Calcium", "Osteoporosis", "Joints", "Arthritis", "Fracture", "Vitamins", "Physical Therapy", "Aging", "Bone Health", "Nutrition", "Exercise", "Checkups", "Hydration", "Sleep", "Weight", "Seasonal", "Prevention", "Vaccines", "Health"]
 category_tag={
-        "mental_health":['depression','mood','routine','wellbeing','therapy','distress', 'tension',
-'anxiety', 'fatigue', 'pressure', 'relaxation','Calm',
-'Peaceful','Tranquil','Serene','Composed','Carefree','easygoing','Laidback'
-'Meditation','Yoga','Vacation','Spa','massage','reading','NatureWalks','Bathing',
-'BreathingExercises','Mindfulness','Aromatherapy','Unwind','health'],
-        "diabetes":['glucose', 'diabetes', 'nutrition', 'insulin', 'hypoglycaemia', 'hyperglycaemia','health'],
-        "heart":['cardio','chestpain', 'heart', 'CVD', 'bloodpressure', 'heartfailure', 'heartattack', 'cardiovasculardiseases','health'],
-        "skeleton":['pain', 'fracture', 'backpain', 'jointpain', 'trauma', 'bone', 'bonedensity', 'boneloss', 'musclepain', 'Sports', 'injury','osteoperosis','calcium','health'],
-        "generic":['routine','wellbeing','vaccines','IntermittentFasting','KetoDiet','Superfoods','OrganicFoods','Vegan','nutrition','worklifebalance','hygienepractices','acupuncture ','minimalism','challenges','lifestyle','healthy','diet','health']
+        "mental_health":["Anxiety", "Stress", "Depression", "Mindfulness", "Wellness", "Mental Health Therapy", "Resilience", "Self-care", "Psychology", "Happiness"],
+        "diabetes":["Glucose", "Insulin", "Diet", "Glycemic", "Fitness", "Foot Care", "Type 1 Diabetes", "Type 2 Diabetes"],
+        "heart":["Cardio", "Heart Diet", "Cholesterol", "Blood Pressure", "Heart Care", "Physical Rehab", "Stress", "Surgery", "Heart Rate", "Heart Health"],
+        "skeleton":["Density", "Calcium", "Osteoporosis", "Joints", "Arthritis", "Fracture", "Vitamins", "Physical Therapy", "Aging", "Bone Health"],
+        "generic":["Nutrition", "Exercise", "Checkups", "Hydration", "Sleep", "Weight", "Seasonal", "Prevention", "Vaccines", "Health","Mindfulness", "Wellness","Resilience", "Self-care", "Psychology", "Happiness"]
     }
